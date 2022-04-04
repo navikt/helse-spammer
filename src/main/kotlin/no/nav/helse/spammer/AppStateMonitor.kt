d1bc8679e4cb46532388f756d33c2b1cf009cea6
David Steinsland <david@steinsland.email>
2022-04-04 21:12:11 +0200

    printer hvilke instanser som har problem

--- src/main/kotlin/no/nav/helse/spammer/AppStateMonitor.kt
+++ src/main/kotlin/no/nav/helse/spammer/AppStateMonitor.kt
@@ -22,6 +22,10 @@ internal class AppStateMonitor(
             validate { it.requireArray("states") {
                 requireKey("app", "state")
                 require("last_active_time", JsonNode::asLocalDateTime)
+                requireArray("instances") {
+                    requireKey("instance", "state")
+                    require("last_active_time", JsonNode::asLocalDateTime)
+                }
             } }
             validate { it.require("threshold", JsonNode::asLocalDateTime) }
             validate { it.require("@opprettet", JsonNode::asLocalDateTime) }
@@ -35,30 +39,36 @@ internal class AppStateMonitor(
     }
 
     override fun onPacket(packet: JsonMessage, context: MessageContext) {
-        if (lastReportTime > LocalDateTime.now().minusMinutes(15)) return // don't create alerts too eagerly
+        val now = LocalDateTime.now()
+        if (lastReportTime > now.minusMinutes(15)) return // don't create alerts too eagerly
         val appsDown = packet["states"]
             .filter { it["state"].asInt() == 0 }
-            .filter { it["last_active_time"].asLocalDateTime() < LocalDateTime.now().minusMinutes(2) }
-            .map { it["app"].asText() to it["last_active_time"].asLocalDateTime() }
+            .filter { it["last_active_time"].asLocalDateTime() < now.minusMinutes(2) }
+            .map { Triple(it["app"].asText(), it["last_active_time"].asLocalDateTime(), it["instances"]
+                .filter { instance -> instance.path("state").asInt() == 0 }
+                .map { instance ->  Pair(instance.path("instance").asText(), instance.path("last_active_time").asLocalDateTime()) }
+            ) }
 
         if (appsDown.isEmpty()) return
 
-        val appString = appsDown.last().let { siste ->
-            if (appsDown.size == 1) siste.printApp()
-            else appsDown.subList(0, appsDown.size - 1).joinToString { it.printApp() } + " og ${siste.printApp()}"
-        }
-        val logtext = String.format(
-            "%s er antatt nede fordi de(n) ikke har svart på ping innen %s",
-            appString,
-            humanReadableTime(ChronoUnit.SECONDS.between(packet["threshold"].asLocalDateTime(), LocalDateTime.now()))
-        )
+        val logtext = String.format("%d app(er) er antatt nede da de(n) ikke svarer tilfredsstillende på ping. Trøblete instanser i :thread:\n%s", appsDown.joinToString { (app, sistAktivitet, _) ->
+            val tid = humanReadableTime(ChronoUnit.SECONDS.between(sistAktivitet, now))
+            "$app (siste aktivitet: $tid - $sistAktivitet)"
+        })
         log.warn(logtext)
-        slackClient?.postMessage(logtext)
-        lastReportTime = LocalDateTime.now()
+        val threadTs = slackClient?.postMessage(logtext)
+        appsDown.forEach { (_, _, instances) ->
+            val text = instances.joinToString(separator = "\n") { (instans, sistAktivitet) ->
+                val tid = humanReadableTime(ChronoUnit.SECONDS.between(sistAktivitet, now))
+                "- $instans (siste aktivitet: $tid - $sistAktivitet)\n"
+            }
+            slackClient?.postMessage(text, threadTs)
+        }
+        lastReportTime = now
     }
 
-    private fun Pair<String, LocalDateTime>.printApp(): String {
+    private fun Triple<String, LocalDateTime, List<Pair<String, LocalDateTime>>>.printApp(): String {
         val tid = humanReadableTime(ChronoUnit.SECONDS.between(second, LocalDateTime.now()))
-        return "$first (sist aktiv: $tid)"
+        return "$first (siste aktivitet: $tid)"
     }
 }
