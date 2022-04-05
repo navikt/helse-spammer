b94084f122e6f7778e47cb41437492e37c34cf06
David Steinsland <david@steinsland.email>
2022-04-05 13:44:02 +0200

    instansene må være inaktive i minst 90 sekunder før varsling

--- src/main/kotlin/no/nav/helse/spammer/AppStateMonitor.kt
+++ src/main/kotlin/no/nav/helse/spammer/AppStateMonitor.kt
@@ -54,6 +54,7 @@ internal class AppStateMonitor(
             .flatMap {
                 it["instances"]
                     .filter { instance -> instance.path("state").asInt() == 0 }
+                    .filter { it["last_active_time"].asLocalDateTime() < now.minusSeconds(90) }
                     .filter { instance ->  instance.path("last_active_time").asLocalDateTime() > now.minusMinutes(20) }
                     .map { instance ->  Pair(instance.path("instance").asText(), instance.path("last_active_time").asLocalDateTime()) }
             }
