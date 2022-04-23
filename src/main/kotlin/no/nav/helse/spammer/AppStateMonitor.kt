a16a3a66b0a48d4c487c392743c000870703920a
David Steinsland <david@steinsland.email>
2022-04-23 20:29:03 +0200

    øker til 70 sekunder for å ta unna endel forventede tregheter

--- src/main/kotlin/no/nav/helse/spammer/AppStateMonitor.kt
+++ src/main/kotlin/no/nav/helse/spammer/AppStateMonitor.kt
@@ -54,7 +54,7 @@ internal class AppStateMonitor(
             .flatMap {
                 it["instances"]
                     .filter { instance -> instance.path("state").asInt() == 0 }
-                    .filter { it["last_active_time"].asLocalDateTime() < now.minusSeconds(60) }
+                    .filter { it["last_active_time"].asLocalDateTime() < now.minusSeconds(70) }
                     .filter { instance ->  instance.path("last_active_time").asLocalDateTime() > now.minusMinutes(20) }
                     .map { instance ->  Pair(instance.path("instance").asText(), instance.path("last_active_time").asLocalDateTime()) }
             }
