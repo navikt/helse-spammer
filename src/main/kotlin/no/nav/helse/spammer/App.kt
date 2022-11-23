db6473a4ef135de62d01a7b31018cf8e0444cc03
David Steinsland <david@steinsland.email>
2022-11-23 12:57:40 +0100

    ikke lage slackmeldinger ved brukerutbetaling mer

--- src/main/kotlin/no/nav/helse/spammer/App.kt
+++ src/main/kotlin/no/nav/helse/spammer/App.kt
@@ -30,7 +30,7 @@ fun main() {
         AvstemmingMonitor(this, slackClient)
         AppStateMonitor(this, slackAlertsClient)
         LoopMonitor(this, slackAlertsClient)
-        BrukerutbetalingMonitor(this, slackClient)
+        BrukerutbetalingMonitor(this, null)
     }.apply {
         register(object : RapidsConnection.StatusListener {
             override fun onStartup(rapidsConnection: RapidsConnection) {
