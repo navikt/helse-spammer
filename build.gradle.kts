6a64546ef70195ca07a1ebe5a2f77d444094698c
david steinsland <david@davidsteinsland.net>
2024-01-11 16:59:59 +0100

    java 21 og google artifact registry 🎉

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,28 +1,40 @@
 
-val jvmTarget = "17"
+val jvmTarget = "21"
 
 val junitJupiterVersion = "5.10.0"
 val testcontainersVersion = "1.19.1"
-val rapidsAndRiversVersion = "2023093008351696055717.ffdec6aede3d"
+val rapidsAndRiversVersion = "2024010209171704183456.6d035b91ffb4"
 val flywayCoreVersion = "9.7.0"
 val hikariCPVersion = "5.0.1"
 val postgresqlVersion = "42.6.0"
 val kotliqueryVersion = "1.9.0"
 val risonVersion = "2.9.10.2"
-val mockkVersion = "1.12.3"
+val mockkVersion = "1.13.9"
 
 plugins {
-    kotlin("jvm") version "1.9.10"
+    kotlin("jvm") version "1.9.22"
 }
 
 repositories {
+    val githubPassword: String? by project
     mavenCentral()
-    maven { url = uri("https://jitpack.io") }
+    /* ihht. https://github.com/navikt/utvikling/blob/main/docs/teknisk/Konsumere%20biblioteker%20fra%20Github%20Package%20Registry.md
+        så plasseres github-maven-repo (med autentisering) før nav-mirror slik at github actions kan anvende førstnevnte.
+        Det er fordi nav-mirroret kjører i Google Cloud og da ville man ellers fått unødvendige utgifter til datatrafikk mellom Google Cloud og GitHub
+     */
+    maven {
+        url = uri("https://maven.pkg.github.com/navikt/maven-release")
+        credentials {
+            username = "x-access-token"
+            password = githubPassword
+        }
+    }
+    maven("https://github-package-registry-mirror.gc.nav.no/cached/maven-release")
 }
 
 dependencies {
     implementation("com.github.navikt:rapids-and-rivers:$rapidsAndRiversVersion")
-    
+
     implementation("org.flywaydb:flyway-core:$flywayCoreVersion")
     implementation("com.zaxxer:HikariCP:$hikariCPVersion")
     implementation("org.postgresql:postgresql:$postgresqlVersion")
@@ -31,19 +43,18 @@ dependencies {
     implementation("com.bazaarvoice.jackson:rison:$risonVersion")
 
     testImplementation("io.mockk:mockk:$mockkVersion")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:$junitJupiterVersion")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
+
+    testImplementation("org.junit.jupiter:junit-jupiter:$junitJupiterVersion")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
     testImplementation("org.testcontainers:postgresql:$testcontainersVersion")
 }
 
 tasks {
-    compileKotlin {
-        kotlinOptions.jvmTarget = jvmTarget
-    }
-    compileTestKotlin {
-        kotlinOptions.jvmTarget = jvmTarget
+    java {
+        toolchain {
+            languageVersion = JavaLanguageVersion.of(jvmTarget)
+        }
     }
 
     named<Jar>("jar") {
@@ -72,6 +83,6 @@ tasks {
     }
 
     withType<Wrapper> {
-        gradleVersion = "8.3"
+        gradleVersion = "8.5"
     }
 }
