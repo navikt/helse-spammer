6703ea30aeec725cfad6a095a42fc021ef263eed
Simen Ullern <simenullern@gmail.com>
2024-03-26 14:55:56 +0100

    Reapply "Hacker inn en ørliten mulighet til å sette channel 💡"
    This reverts commit 7bd560a8ad7ec3389ba408a75bfdb08fa64b663a.

--- src/main/kotlin/no/nav/helse/spammer/SlackClient.kt
+++ src/main/kotlin/no/nav/helse/spammer/SlackClient.kt
@@ -44,9 +44,9 @@ internal class SlackClient(private val accessToken: String, private val channel:
             .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
     }
 
-    fun postMessage(text: String, threadTs: String? = null, broadcast: Boolean = false): String? {
+    fun postMessage(text: String, threadTs: String? = null, broadcast: Boolean = false, customChannel: String? = null): String? {
         return "https://slack.com/api/chat.postMessage".post(objectMapper.writeValueAsString(mutableMapOf<String, Any>(
-            "channel" to channel,
+            "channel" to (customChannel ?: channel),
             "text" to text
         ).apply {
             threadTs?.also {
