cf7ed18ec0feb7f85bd891a9fc6ec551bbe1b56e
david steinsland <david@davidsteinsland.net>
2024-11-06 13:17:28 +0100

    endrer til å lytte på kvitteringene fra OS

--- src/main/kotlin/no/nav/helse/spammer/UtbetalingMonitor.kt
+++ src/main/kotlin/no/nav/helse/spammer/UtbetalingMonitor.kt
@@ -1,5 +1,6 @@
 package no.nav.helse.spammer
 
+import com.fasterxml.jackson.databind.JsonNode
 import no.nav.helse.rapids_rivers.*
 import org.slf4j.LoggerFactory
 import kotlin.time.ExperimentalTime
@@ -16,36 +17,30 @@ internal class UtbetalingMonitor(
 
     init {
         River(rapidsConnection).apply {
-            validate { it.demandValue("@event_name", "vedtaksperiode_endret") }
-            validate { it.requireKey("aktørId") }
-            validate { it.requireKey("fødselsnummer") }
-            validate { it.requireKey("organisasjonsnummer") }
-            validate { it.requireKey("vedtaksperiodeId") }
-            validate { it.requireKey("@opprettet") }
-            validate { it.demandValue("gjeldendeTilstand", "UTBETALING_FEILET") }
+            validate { it.demandValue("@event_name", "transaksjon_status") }
+            validate { it.require("@opprettet", JsonNode::asLocalDateTime) }
+            validate { it.requireKey("utbetalingId", "beskrivelse") }
+            validate { it.interestedIn("kodemelding") }
+            validate { it.requireAny("status", listOf("AVVIST", "FEIL")) }
         }.register(UtbetalingFeilet(slackClient, slackThreadDao))
     }
 
     private class UtbetalingFeilet(private val slackClient: SlackClient?, private val slackThreadDao: SlackThreadDao?): River.PacketListener {
         override fun onError(problems: MessageProblems, context: MessageContext) {
-            sikkerLog.error("forstod ikke utbetaling_feilet:\n${problems.toExtendedReport()}")
+            sikkerLog.error("forstod ikke transaksjon_status:\n${problems.toExtendedReport()}")
         }
 
         override fun onPacket(packet: JsonMessage, context: MessageContext) {
             if (slackThreadDao == null) return
             slackClient?.postMessage(
-                slackThreadDao,
-                packet["vedtaksperiodeId"].asText(),
                 String.format(
-                    "Utbetaling for vedtaksperiode <%s|%s> (<%s|tjenestekall>) feilet!",
-                    Kibana.createUrl(String.format("\"%s\"", packet["vedtaksperiodeId"].asText()), packet["@opprettet"].asLocalDateTime().minusHours(1)),
-                    packet["vedtaksperiodeId"].asText(),
-                    Kibana.createUrl(
-                        String.format("\"%s\"", packet["vedtaksperiodeId"].asText()),
-                        packet["@opprettet"].asLocalDateTime().minusHours(1),
-                        null,
-                        "tjenestekall-*"
-                    )
+                    "Utbetaling <%s|%s> (<%s|tjenestekall>) feilet med status %s! Beskrivelse: %s%s)",
+                    Kibana.createUrl(String.format("\"%s\"", packet["utbetalingId"].asText()), packet["@opprettet"].asLocalDateTime().minusHours(1)),
+                    packet["utbetalingId"].asText(),
+                    Kibana.createUrl(String.format("\"%s\"", packet["vedtaksperiodeId"].asText()), packet["@opprettet"].asLocalDateTime().minusHours(1), null, "tjenestekall-*"),
+                    packet["status"].asText(),
+                    packet["beskrivelse"].asText(),
+                    packet["kodemelding"].asText()?.let { " ($it)" }
                 )
             )
         }
